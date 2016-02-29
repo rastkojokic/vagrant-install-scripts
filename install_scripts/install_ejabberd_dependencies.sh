d064edd734c3a48c95b8f6c518796398382a0162
Rastko Jokic <rastkojokic@gmail.com>
2016-02-29 14:53:29 +0100

    Add install_ejabberd_dependencies script
    Script for install ejabberd dependencies along with newest erlang OTP version

---
+++ install_scripts/install_ejabberd_dependencies.sh
@@ -0,0 +1,17 @@
+sudo apt-get install erlang erlang-doc -y
+sudo apt-get install libyaml-dev -y
+sudo apt-get install libexpat1-dev -y
+sudo apt-get install automake -y
+sudo apt-get install libssl-dev -y
+sudo apt-get install build-essential -y
+
+wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+sudo dpkg -i erlang-solutions_1.0_all.deb
+
+sudo sh -c "echo 'deb http://packages.erlang-solutions.com/ubuntu trusty contrib' >> /etc/apt/sources.list"
+
+wget http://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc
+sudo apt-key add erlang_solutions.asc
+
+sudo apt-get update
+sudo apt-get install erlang

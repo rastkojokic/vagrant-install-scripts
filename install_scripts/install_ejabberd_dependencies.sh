882df7a8d7460ed748299db750490622fa6a5ac8
Rastko Jokic <rastkojokic@gmail.com>
2016-03-07 08:48:12 +0100

    Update install_ejabberd_dependencies.sh

--- install_scripts/install_ejabberd_dependencies.sh
+++ install_scripts/install_ejabberd_dependencies.sh
@@ -1,17 +1,17 @@
-sudo apt-get install erlang erlang-doc -y
-sudo apt-get install libyaml-dev -y
-sudo apt-get install libexpat1-dev -y
-sudo apt-get install automake -y
-sudo apt-get install libssl-dev -y
-sudo apt-get install build-essential -y
+sudo apt-get install erlang erlang-doc -y --force-yes
+sudo apt-get install libyaml-dev -y --force-yes
+sudo apt-get install libexpat1-dev -y --force-yes
+sudo apt-get install automake -y --force-yes
+sudo apt-get install libssl-dev -y --force-yes
+sudo apt-get install build-essential -y --force-yes
 
-wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+sudo wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
 sudo dpkg -i erlang-solutions_1.0_all.deb
 
 sudo sh -c "echo 'deb http://packages.erlang-solutions.com/ubuntu trusty contrib' >> /etc/apt/sources.list"
 
-wget http://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc
+sudo wget http://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc
 sudo apt-key add erlang_solutions.asc
 
 sudo apt-get update
-sudo apt-get install erlang -y
+sudo apt-get install erlang -y --force-yes

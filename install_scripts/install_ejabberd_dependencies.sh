dc2cbe867e1c0fb1df8168ab6e8dc6ba0ff061df
Rastko Jokic <rastko@thepodyum.com>
2016-10-27 10:51:53 +0200

    Update ejabberd script to install Erlang 18

--- install_scripts/install_ejabberd_dependencies.sh
+++ install_scripts/install_ejabberd_dependencies.sh
@@ -5,8 +5,9 @@ sudo apt-get install automake -y --force-yes
 sudo apt-get install libssl-dev -y --force-yes
 sudo apt-get install build-essential -y --force-yes
 
-sudo wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
-sudo dpkg -i erlang-solutions_1.0_all.deb
+# sudo wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+sudo wget http://packages.erlang-solutions.com/site/esl/esl-erlang/FLAVOUR_1_general/esl-erlang_18.1-1~ubuntu~trusty_amd64.deb
+sudo dpkg -i esl-erlang_18.1-1~ubuntu~trusty_amd64.deb
 
 sudo sh -c "echo 'deb http://packages.erlang-solutions.com/ubuntu trusty contrib' >> /etc/apt/sources.list"
 

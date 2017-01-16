a15c162aaef8260ddc8aa9dca93a9be3bfee353a
Rastko Jokic <rastko@thepodyum.com>
2017-01-16 10:37:48 -0500

    Update mongo

--- install_scripts/install_mongodb.sh
+++ install_scripts/install_mongodb.sh
@@ -1,11 +1,7 @@
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
 sudo apt-get update
 
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9ECBEC467F0CEB10
-
-sudo apt-get update
-
-sudo apt-get install -y  --force-yes mongodb-org
+sudo apt-get install -y mongodb-org=3.2.11 mongodb-org-server=3.2.11 mongodb-org-shell=3.2.11 mongodb-org-mongos=3.2.11 mongodb-org-tools=3.2.11

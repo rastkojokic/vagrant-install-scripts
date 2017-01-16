0757db78cb8f82caf62e7be46e380796574a6ca2
Rastko Jokic <rastko@thepodyum.com>
2017-01-16 09:48:08 -0500

    Update mongo db to 3.2

--- install_scripts/install_mongodb.sh
+++ install_scripts/install_mongodb.sh
@@ -1,6 +1,6 @@
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/testing multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
 sudo apt-get update
 
